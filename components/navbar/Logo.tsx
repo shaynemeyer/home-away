18b2b944d0b2760fb6387406468a62ae74774741
Shayne Meyer <shayne.meyer@gmail.com>
2024-07-19 14:15:51 -0700

    added darkmode switcher to application

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,4 +1,14 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { LuTent } from "react-icons/lu";
+
 function Logo() {
-  return <div>Logo</div>;
+  return (
+    <Button size="icon" asChild>
+      <Link href="/">
+        <LuTent className="w-6 h-6" />
+      </Link>
+    </Button>
+  );
 }
 export default Logo;
