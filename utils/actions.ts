ca79025a1270a929ec36697cf45ca126d84e200e
Shayne Meyer <shayne.meyer@gmail.com>
2024-07-21 05:39:44 -0700

    added update profile functionality

--- utils/actions.ts
+++ utils/actions.ts
@@ -6,16 +6,37 @@ import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
+const getAuthUser = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    throw new Error("You must be logged in to access this route");
+  }
+
+  if (!user.privateMetadata.hasProfile) redirect("/profile/create");
+
+  return user;
+};
+
+const renderError = (error: unknown): { message: string } => {
+  console.log(error);
+  return {
+    message: error instanceof Error ? error.message : "An error occurred",
+  };
+};
+
 export const createProfileAction = async (
   prevState: any,
   formData: FormData
 ) => {
   try {
     const user = await currentUser();
+
     if (!user) throw new Error("Please login to create a profile");
 
     const rawData = Object.fromEntries(formData);
     const validatedFields = profileSchema.parse(rawData);
+
     await db.profile.create({
       data: {
         clerkId: user.id,
@@ -24,16 +45,16 @@ export const createProfileAction = async (
         ...validatedFields,
       },
     });
+
     await clerkClient.users.updateUserMetadata(user.id, {
       privateMetadata: {
         hasProfile: true,
       },
     });
   } catch (error) {
-    return {
-      message: error instanceof Error ? error.message : "An error occurred",
-    };
+    return renderError(error);
   }
+
   redirect("/");
 };
 
@@ -49,5 +70,45 @@ export const fetchProfileImage = async () => {
       profileImage: true,
     },
   });
+
   return profile?.profileImage;
+};
+
+export const fetchProfile = async () => {
+  const user = await getAuthUser();
+
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+  });
+
+  if (!profile) return redirect("/profile/create");
+
+  return profile;
+};
+
+export const updateProfileAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  const user = await getAuthUser();
+  try {
+    const rawData = Object.fromEntries(formData);
+
+    const validatedFields = profileSchema.parse(rawData);
+
+    await db.profile.update({
+      where: {
+        clerkId: user.id,
+      },
+      data: validatedFields,
+    });
+
+    revalidatePath("/profile");
+
+    return { message: "Profile updated successfully" };
+  } catch (error) {
+    return renderError(error);
+  }
 };
