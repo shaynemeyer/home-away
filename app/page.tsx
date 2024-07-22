4dff6135ea3d7bc528aa8345ce54cc35e0a938b3
Shayne Meyer <shayne.meyer@gmail.com>
2024-07-22 06:33:58 -0700

    added PropertyCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
+import { Suspense } from "react";
 import CategoriesList from "@/components/home/CategoriesList";
 import PropertiesContainer from "@/components/home/PropertiesContainer";
-import { Button } from "@/components/ui/button";
+import LoadingCards from "@/components/card/LoadingCards";
 
 export default function HomePage({
   searchParams,
@@ -16,10 +17,12 @@ export default function HomePage({
         category={searchParams?.category}
         search={searchParams?.search}
       />
-      <PropertiesContainer
-        category={searchParams?.category}
-        search={searchParams?.search}
-      />
+      <Suspense fallback={<LoadingCards />}>
+        <PropertiesContainer
+          category={searchParams?.category}
+          search={searchParams?.search}
+        />
+      </Suspense>
     </section>
   );
 }
--- components/home/PropertiesList.tsx
+++ components/home/PropertiesList.tsx
@@ -1,4 +1,13 @@
-function PropertiesList() {
-  return <div>PropertiesList</div>;
+import PropertyCard from "@/components/card/PropertyCard";
+import type { PropertyCardProps } from "@/utils/types";
+
+function PropertiesList({ properties }: { properties: PropertyCardProps[] }) {
+  return (
+    <section className="mt-4 gap-8 grid sm:grid-cols-2  lg:grid-cols-3  xl:grid-cols-4">
+      {properties.map((property) => {
+        return <PropertyCard key={property.id} property={property} />;
+      })}
+    </section>
+  );
 }
 export default PropertiesList;
