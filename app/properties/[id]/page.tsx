ae11521f3297e1a0b89b491ae139073b728a964a
Shayne Meyer <shayne.meyer@gmail.com>
2024-07-23 13:03:33 -0700

    added Calendar component

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,5 +1,6 @@
 import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
 import PropertyRating from "@/components/card/PropertyRating";
+import BookingCalendar from "@/components/properties/booking/BookingCalendar";
 import BreadCrumbs from "@/components/properties/BreadCrumbs";
 import ImageContainer from "@/components/properties/ImageContainer";
 import ShareButton from "@/components/properties/ShareButton";
@@ -33,7 +34,7 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
           </div>
         </div>
         <div className="lg:col-span-4 flex flex-col items-center">
-          {/* calendar */}
+          <BookingCalendar />
         </div>
       </section>
     </section>
