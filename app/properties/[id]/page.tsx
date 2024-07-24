0a1477d23f60daaa420aba50967ce4d4d19e296a
Shayne Meyer <shayne.meyer@gmail.com>
2024-07-24 07:41:34 -0700

    added Amenties component

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,5 +1,6 @@
 import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
 import PropertyRating from "@/components/card/PropertyRating";
+import Amenities from "@/components/properties/Amenities";
 import BookingCalendar from "@/components/properties/booking/BookingCalendar";
 import BreadCrumbs from "@/components/properties/BreadCrumbs";
 import Description from "@/components/properties/Description";
@@ -43,6 +44,7 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
           <UserInfo profile={{ firstName, profileImage }} />
           <Separator className="mt-4" />
           <Description description={property.description} />
+          <Amenities amenities={property.amenities} />
         </div>
         <div className="lg:col-span-4 flex flex-col items-center">
           <BookingCalendar />
