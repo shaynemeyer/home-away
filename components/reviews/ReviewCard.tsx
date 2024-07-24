fa55ddde2524234472ca837099900a1bc93f0fc9
Shayne Meyer <shayne.meyer@gmail.com>
2024-07-24 12:54:59 -0700

    added ReviewCard component

--- components/reviews/ReviewCard.tsx
+++ components/reviews/ReviewCard.tsx
@@ -1,4 +1,14 @@
-function ReviewCard() {
+type ReviewCardProps = {
+  reviewInfo: {
+    comment: string;
+    rating: number;
+    name: string;
+    image: string;
+  };
+  children?: React.ReactNode;
+};
+
+function ReviewCard({ reviewInfo, children }: ReviewCardProps) {
   return <div>ReviewCard</div>;
 }
 export default ReviewCard;
