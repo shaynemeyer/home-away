5cbc31718f9024e035a40440c47d6d28b071dcde
Shayne Meyer <shayne.meyer@gmail.com>
2024-07-24 07:33:19 -0700

    added PropertyDetails and UserInfo

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -3,7 +3,9 @@ import PropertyRating from "@/components/card/PropertyRating";
 import BookingCalendar from "@/components/properties/booking/BookingCalendar";
 import BreadCrumbs from "@/components/properties/BreadCrumbs";
 import ImageContainer from "@/components/properties/ImageContainer";
+import PropertyDetails from "@/components/properties/PropertyDetails";
 import ShareButton from "@/components/properties/ShareButton";
+import UserInfo from "@/components/properties/UserInfo";
 import { fetchPropertyDetails } from "@/utils/actions";
 import { redirect } from "next/navigation";
 
@@ -14,6 +16,9 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
 
   const { baths, bedrooms, beds, guests } = property;
   const details = { baths, bedrooms, beds, guests };
+
+  const firstName = property.profile.firstName;
+  const profileImage = property.profile.profileImage;
 
   return (
     <section>
@@ -32,6 +37,8 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
             <h1 className="text-xl font-bold">{property.name}</h1>
             <PropertyRating inPage propertyId={property.id} />
           </div>
+          <PropertyDetails details={details} />
+          <UserInfo profile={{ firstName, profileImage }} />
         </div>
         <div className="lg:col-span-4 flex flex-col items-center">
           <BookingCalendar />
