d253c5abd65801a39a9dee7d39d8976923ef9ef7
Shayne Meyer <shayne.meyer@gmail.com>
2024-07-24 08:20:03 -0700

    updated NavSearch

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,13 +1,12 @@
 "use client";
 import { Input } from "@/components/ui/input";
-import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 import { useDebouncedCallback } from "use-debounce";
 import { useState, useEffect } from "react";
 
 function NavSearch() {
   const searchParams = useSearchParams();
 
-  const pathname = usePathname();
   const { replace } = useRouter();
 
   const [search, setSearch] = useState(
@@ -21,7 +20,7 @@ function NavSearch() {
     } else {
       params.delete("search");
     }
-    replace(`${pathname}?${params.toString()}`);
+    replace(`/?${params.toString()}`);
   }, 300);
 
   useEffect(() => {
