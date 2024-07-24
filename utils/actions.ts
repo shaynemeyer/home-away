8c529b59ce46b3f6aacb3a02312d0112391c7f18
Shayne Meyer <shayne.meyer@gmail.com>
2024-07-24 12:48:03 -0700

    added Save Review Action

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import {
+  createReviewSchema,
   imageSchema,
   profileSchema,
   propertySchema,
@@ -284,8 +285,29 @@ export const fetchPropertyDetails = (id: string) => {
   });
 };
 
-export const createReviewAction = async () => {
-  return { message: "create review" };
+export const createReviewAction = async (
+  prevState: any,
+  formData: FormData
+) => {
+  const user = await getAuthUser();
+  try {
+    const rawData = Object.fromEntries(formData);
+
+    const validatedFields = validateWithZodSchema(createReviewSchema, rawData);
+
+    await db.review.create({
+      data: {
+        ...validatedFields,
+        profileId: user.id,
+      },
+    });
+
+    revalidatePath(`/properties/${validatedFields.propertyId}`);
+
+    return { message: "Review submitted successfully" };
+  } catch (error) {
+    return renderError(error);
+  }
 };
 
 export const fetchPropertyReviews = async () => {
